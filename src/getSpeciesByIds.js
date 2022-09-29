75a1ca3a7da3f8147e481796c502f6f25303107a
Rômulo Freitas <110496787+romulorfreitas@users.noreply.github.com>
2022-09-29 16:15:09 -0400

    Refatora requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -8,8 +8,7 @@ function getSpeciesByIds(...ids) {
     } return ids.includes(specie.id);
   });
 }
-console.log(getSpeciesByIds('e8481c1d-42ea-4610-8e11-1752cfc05a46'));
-
+// console.log(getSpeciesByIds());
 // documentacao https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
 
 module.exports = getSpeciesByIds;
