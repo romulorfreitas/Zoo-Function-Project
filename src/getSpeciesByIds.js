39c4adff94cf12bb8f663a1b1d93df8250b8c4ae
Rômulo Freitas <110496787+romulorfreitas@users.noreply.github.com>
2022-09-29 15:47:46 -0400

    Entrega requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,16 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return species.filter((specie) => {
+    if (ids === 'undefined') {
+      return [];
+    } return ids.includes(specie.id);
+  });
 }
+
+console.log(getSpeciesByIds('e8481c1d-42ea-4610-8e11-1752cfc05a46'));
+
+//documentacao https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
 
 module.exports = getSpeciesByIds;
