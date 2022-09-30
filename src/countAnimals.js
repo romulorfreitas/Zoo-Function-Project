812fb02bbeac79f34c91179efe02e44219ed43cf
Rômulo Freitas <110496787+romulorfreitas@users.noreply.github.com>
2022-09-30 17:57:18 -0400

    Entrega requisitos 06, 07 e 15

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,22 +1,22 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const [ratin, quantos] = species;
-const teste2 = ratin.name;
-const teste3 = quantos.residents.length;
+// const [ratin, quantos] = species;
+// const teste2 = ratin.name;
+// const teste3 = quantos.residents.length;
 
-const [{ ...animais }] = species;
-// console.log(animais);
+// const [{ ...animais }] = species;
+// // console.log(animais);
 
-// console.log(teste2);
-// console.log(teste3);
-// console.log(lion);
+// // console.log(teste2);
+// // console.log(teste3);
+// // console.log(lion);
 
-function countAnimals(animal) {
-  if (!animal) {
-    return species.find((s) => s.name === animal).residents.length;
-  } return {};
-}
-console.log(countAnimals());
+// function countAnimals(animal) {
+//   if (!animal) {
+//     return species.find((s) => s.name === animal).residents.length;
+//   } return {};
+// }
+// console.log(countAnimals());
 
-// module.exports = countAnimals;
+module.exports = countAnimals;
