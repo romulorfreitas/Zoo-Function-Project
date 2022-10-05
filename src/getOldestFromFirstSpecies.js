ee2aaa9b078f7d71c696ad2cf192940456bd371c
Rômulo Freitas <110496787+romulorfreitas@users.noreply.github.com>
2022-10-05 14:52:18 -0400

    Entrega requisito 10 e parte da 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const fisrtResp = employees.find((employee) => employee.id === id).responsibleFor[0]; // const para pegar o primeiro animal que o empregado e responsavel.
+  // console.log(fisrtResp);
+  const verifyAge = species.find((specie) => specie.id === fisrtResp).residents; // const para buscar os residentes do id que for igual ao do inserido no parametro.
+  const older = verifyAge.sort((a, b) => b.age - a.age)[0]; // const para ordenar os animais pela idade, do animal mais velhor para o mais novo, acessando o primeiro.
+  if (id !== undefined) { // caso o parametro id receba um valor, retorna o value da const older, o animal mais velho.
+    return Object.values(older);
+  }
 }
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,38 @@
+const { species, hours } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+
+// const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+// const [lion] = species.map((d) => d.availability)
+// const [, tiger] = species.map((d) => d.availability)
+// const [, , bear] = species.map((d) => d.availability)
+// const [, , , penguin] = species.map((d) => d.availability)
+// const [, , , , otter] = species.map((d) => d.availability)
+// const [, , , , , frog] = species.map((d) => d.availability)
+// const [, , , , , , snakes] = species.map((d) => d.availability)
+// const [, , , , , , , elephant] = species.map((d) => d.availability)
+// const [, , , , , , , , giraffes] = species.map((d) => d.availability)
+
+// const schedulee = species.map((t) => t.availability)
+// console.log(schedulee);
+const findAnimals = species.map((a) => a.name);
+const [teste] = hours;
+console.log(teste);
+// const findAnimals2 = species.map((a) => a.name).find((as) => as);
+// const days = species.map((day) => day.availability)
+// console.log(days);
+// const animals = species.map((animal) => animal.name)
+// console.log(animals);
+// const times = hours
+// console.log(times);
+// const daysTime = Object.keys(data.hours)
+// console.log(daysTime);
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
+  if (findAnimals.find((sa) => sa === scheduleTarget)) {
+    return (species.find((specie) => specie.name === scheduleTarget).availability);
+  }
 }
-
+// console.log(getSchedule('bears'));
 module.exports = getSchedule;
